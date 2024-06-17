70fb1a1195935d886404dd502e6fabdca71eb861
Edward Mizuka <mizuka.wu@outlook.com>
2024-06-17 16:21:10 +0800

    feat: 添加体积输出

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,5 +1,13 @@
-export default {
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+export default defineConfig({
     build: {
         target: 'esnext'
-    }
-}
+    },
+    plugins: [
+        visualizer({
+            gzipSize: true,
+            brotliSize: true
+        })
+    ]
+})
