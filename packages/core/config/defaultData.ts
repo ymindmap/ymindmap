e17f656e7c6d0125c2e36b91d62d2fe15214ee13
Edward Mizuka <mizuka.wu@outlook.com>
2024-06-17 20:38:44 +0800

    feat: 调整默认的data

--- packages/core/config/defaultData.ts
+++ packages/core/config/defaultData.ts
@@ -1,6 +1,6 @@
 import { js2xml } from 'xml-js';
 
-export function getDefaultData(defaultTopic = '请输入内容') {
+export function getDefaultData() {
     return js2xml({
         "mindmap": {
             "topic": [
@@ -8,19 +8,16 @@ export function getDefaultData(defaultTopic = '请输入内容') {
                     "_attributes": {
                         "id": "root",
                     },
-                    "_text": defaultTopic,
-                    // "topic": [
-                    //     {
-                    //         "_attributes": {
-                    //         },
-                    //         "_text": defaultTopic
-                    //     },
-                    //     {
-                    //         "_attributes": {
-                    //         },
-                    //         "_text": defaultTopic
-                    //     },
-                    // ]
+                    "topic": [
+                        {
+                            "_attributes": {
+                            },
+                        },
+                        {
+                            "_attributes": {
+                            },
+                        },
+                    ]
                 },
             ]
         }
