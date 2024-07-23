171f9ce0581fbe912973689ad9e648c5df5caf17
Edward Mizuka <mizuka.wu@outlook.com>
2024-07-23 23:33:45 +0800

    feat ✨: Add board view property to DOM container

--- packages/browser/dom/index.ts
+++ packages/browser/dom/index.ts
@@ -5,6 +5,8 @@ import type { LeaferCanvas } from 'leafer-ui'
 
 export const CANVAS_CONTAINER_MAP = new Map<LeaferCanvas, HTMLDivElement>();
 
+export const DOM_KEY = '__board__'
+
 export function getElement(canvas: LeaferCanvas): HTMLDivElement {
     // 自动生成绑定的dom元素
     if (canvas.view.parentElement) return canvas.view.parentElement as HTMLDivElement;
@@ -13,11 +15,13 @@ export function getElement(canvas: LeaferCanvas): HTMLDivElement {
     CANVAS_CONTAINER_MAP.set(canvas, container);
 
     container.appendChild(canvas.view);
+    Reflect.set(container, DOM_KEY, canvas.view);
     return container;
 }
 
 export function destroy(canvas: LeaferCanvas) {
     if (canvas.view.parentElement) {
+        Reflect.deleteProperty(canvas.view.parentElement, DOM_KEY);
         canvas.view.parentElement.parentElement?.removeChild(canvas.view.parentElement);
         CANVAS_CONTAINER_MAP.delete(canvas);
         canvas.destroy();
